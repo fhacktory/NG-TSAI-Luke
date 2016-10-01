633709dfe3707832d7592a2a58193c7cabf4ea58
Luc Gardon <gardon.luc@gmail.com>
2016-10-01 17:14:49 +0200

    reformat

--- pwoned.helper.js
+++ pwoned.helper.js
@@ -1,14 +1,14 @@
-'use strict'
+'use strict';
 
 const path = process.cwd();
-const Pwoned = require(path + '/pwoned.model.js')
+const Pwoned = require(path + '/pwoned.model.js');
 
-const create  =  (username, email) =>{
-        var user = {
-            username: username,
-            email: email
-        };
-        Pwoned.create(user)
-}
+const create = (username, email) => {
+    var user = {
+        username: username,
+        email: email
+    };
+    Pwoned.create(user)
+};
 
 module.exports = create;
