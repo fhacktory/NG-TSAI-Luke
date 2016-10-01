83837508c445846cda0dfa09cb17289ef537b769
quentinbt <quentin.basset@gmail.com>
2016-10-01 17:45:57 +0200

    helper updated

--- pwoned.helper.js
+++ pwoned.helper.js
@@ -1,14 +1,28 @@
-'use strict';
+'use strict'
 
 const path = process.cwd();
-const Pwoned = require(path + '/pwoned.model.js');
+const Pwoned = require(path + '/pwoned.model.js')
 
-const create = (username, email) => {
+const create  =  (username, email) =>{
     var user = {
         username: username,
         email: email
     };
     Pwoned.create(user)
-};
+}
 
+const addPoint = (username, points) => {
+    const condition = {username: username},
+        update = {$inc: {points: points}}
+    Pwoned.update(conditions, update, options, callback)
+}
+
+const getUser = (username) => {
+    Pwoned
+        .find({username: username}
+            .exec((err, result) => return result)
+        }
+}
 module.exports = create;
+module.exports = addPoint;
+module.exports = getUser;
