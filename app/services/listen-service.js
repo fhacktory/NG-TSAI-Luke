c7c034f398046c20cf4b01b3a4ae106562c471c0
Luc Gardon <gardon.luc@gmail.com>
2016-10-02 03:16:58 +0200

    Add drone fly

--- app/services/listen-service.js
+++ app/services/listen-service.js
@@ -2,50 +2,54 @@ const userService = require('./user-service');
 const Pwoned = require('../models/pwoned.model.js');
 const pointService = require('./point-service');
 
+const arDrone = require('ar-drone');
+const client = arDrone.createClient();
+
 var player = require('play-sound')(opts = {})
 var last = [];
 
 module.exports = {
-    listenToSuze: function(message) {
-		var regex = new RegExp('<@(\\w*)>');
+    listenToSuze: function (message) {
+        var regex = new RegExp('<@(\\w*)>');
 
-		if (message.text && message.text.indexOf("suze") === 0) {
-			var pwner = regex.exec(message.text);
-			if (pwner !== null && pwner[0] !== null) {
-				// pwner[0] : Login avec <@ ... >
-				// pwner[1] : Id du user
+        if (message.text && message.text.indexOf("suze") === 0) {
+            var pwner = regex.exec(message.text);
+            if (pwner !== null && pwner[0] !== null) {
+                // pwner[0] : Login avec <@ ... >
+                // pwner[1] : Id du user
+                makeItDanceBaby();
 
-				player.play('assets/wasted.mp3', function(err){
-				    console.log(err);
-				}); // $ mplayer foo.mp3  
+                player.play('assets/wasted.mp3', function (err) {
+                    console.log(err);
+                }); // $ mplayer foo.mp3
 
-				rtm.sendMessage(userService.getUserById(message.user).name+" s'est fait pwed par "+userService.getUserById(pwner[1]).name, message.channel);
-				pointService.getPointToTransfert(userService.getUserById(message.user).name, userService.getUserById(pwner[1]).name);
-				return true;
-			}
-		}
-		return false;
-	},
-	listenRandom: function(message) {
-		var result = Math.floor((Math.random() * 10) + 1);
+                rtm.sendMessage(userService.getUserById(message.user).name + " s'est fait pwed par " + userService.getUserById(pwner[1]).name, message.channel);
+                pointService.getPointToTransfert(userService.getUserById(message.user).name, userService.getUserById(pwner[1]).name);
+                return true;
+            }
+        }
+        return false;
+    },
+    listenRandom: function (message) {
+        var result = Math.floor((Math.random() * 10) + 1);
 
-		if (result === 10) {
-			rtm.sendMessage("Ta gueule <@"+message.user+"> !", message.channel);
-		}
-	},
-	listenAlone: function(message) {
-		var nbMessage = 0;
-		var lastMessage = 0;
+        if (result === 10) {
+            rtm.sendMessage("Ta gueule <@" + message.user + "> !", message.channel);
+        }
+    },
+    listenAlone: function (message) {
+        var nbMessage = 0;
+        var lastMessage = 0;
 
-		for (i = 0; i < 10; i++) {
-    		if (last[i] === message.user) {
-    			if (i < 3) {
-    				lastMessage ++;
-    			}
-    			nbMessage ++;
-    		}
-  		}
-  		last.unshift(message.user);
+        for (i = 0; i < 10; i++) {
+            if (last[i] === message.user) {
+                if (i < 3) {
+                    lastMessage++;
+                }
+                nbMessage++;
+            }
+        }
+        last.unshift(message.user);
 
 		if (nbMessage >= 8 && lastMessage === 3) {
 			rtm.sendMessage("Tu parles seul <@"+message.user+"> !", message.channel);
@@ -69,4 +73,22 @@ module.exports = {
 			    });
 		}
 	}
+}
+
+function makeItDanceBaby() {
+    client.takeoff();
+
+    client
+        .after(3000, function () {
+            this.up(0.5);
+            this.animateLeds('blinkRed', 5, 2)
+        })
+        .after(3000, function() {
+            this.stop();
+            this.animate('yawShake', 6000);
+        })
+        .after(8000, function () {
+            this.stop();
+            this.land();
+        });
 }
