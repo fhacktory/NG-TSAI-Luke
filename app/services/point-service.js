443c7a7d7492ea4eeb2dbce942e8a4e5a3371a9e
cabane_g <gabriel.cabanes@epitech.eu>
2016-10-02 02:23:04 +0200

    math.ceil

--- app/services/point-service.js
+++ app/services/point-service.js
@@ -5,8 +5,8 @@ const pwonedHelper = require('../../pwoned.helper');
 module.exports = {
     getPointToTransfert: function (noob, ninja) {
         const changePoint = (result, err) => {
-            pwonedHelper.addPoint(ninja, result.points * 10/100)
-            pwonedHelper.addPoint(noob, result.points * 10/100 * -1)
+            pwonedHelper.addPoint(ninja, noob, Math.ceil(result.points * 10/100))
+            pwonedHelper.addPoint(noob, ninja, Math.ceil(result.points * 10/100 * -1))
         }
         pwonedHelper.getUser(noob).then(changePoint)
     }
