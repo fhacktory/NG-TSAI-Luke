570a44048498f781dfe2dc94c7bbe80c3ccc4558
Luc Gardon <gardon.luc@gmail.com>
2016-10-02 04:01:30 +0200

    refactor component

--- front/app/components/rankings.jsx
+++ front/app/components/rankings.jsx
@@ -1,23 +1,10 @@
 import React from 'react';
-import Avatar from 'material-ui/Avatar';
-import {List, ListItem} from 'material-ui/List';
-import ActionInfo from 'material-ui/svg-icons/action/info';
 
-const style = {
-    maxWidth: 300
-};
+import RankingList from './rankingList.jsx';
 
 const Rankings = ({rankings}) => (
-    <List style={style}>
-        {rankings.map((rank, i) =>
-            <ListItem key={rank._id}
-                      leftAvatar={<Avatar src={rank.img_24}/>}
-                      primaryText={`N°: ${i + 1} - ${rank.username}`}
-                      secondaryText={rank.points + ' points'}
-                      rightIcon={<ActionInfo />}
-            />
-        )}
-    </List>
+    <RankingList rankings={rankings}>
+    </RankingList>
 );
 
 export default Rankings;
